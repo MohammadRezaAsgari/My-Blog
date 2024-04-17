a88a24eb3d71de6001a5bb1e901abda76f573a7c
MohammadRezaAsgari <m.asgari1589@gmail.com>
2024-04-17 18:40:04 +0330

    add models of core app

--- MyBlog/core/models.py
+++ MyBlog/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20,unique=True)
+    desc = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    date_created = models.TimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.TimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
